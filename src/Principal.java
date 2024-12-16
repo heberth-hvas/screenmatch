64c12a682525c6a5896d832a3690a6810b224baa
Heberth Severo <hvinicius150@gmail.com>
2024-12-16 11:22:47 -0300

    Final commit

--- src/Principal.java
+++ src/Principal.java
@@ -1,15 +1,54 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("Os incríveis");
+        meuFilme.setAnoDeLancamento(2003);
+        meuFilme.setDuracaoEmMinutos(115);
+
+        meuFilme.exibeFichaTecnica();
+
+        meuFilme.avalia(8);
+        meuFilme.avalia(7);
+        meuFilme.avalia(8);
+
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+
+        System.out.println(meuFilme.pegaMedia());
+
+        Serie friends = new Serie();
+        friends.setNome("Friends");
+        friends.setAnoDeLancamento(1994);
+        friends.setTemporadas(10);
+        friends.setEpisodiosPorTemporada(21);
+        friends.setMinutosPorEpisodio(22);
+        friends.exibeFichaTecnica();
+        System.out.println("Duração da Série: " + friends.getDuracaoEmMinutos());
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Vingadores");
+        outroFilme.setAnoDeLancamento(2012);
+        outroFilme.setDuracaoEmMinutos(140);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(friends);
+        System.out.println("NOVO " + calculadora.getTempoTotal());
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(meuFilme);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(friends);
+        episodio.setTotalVisualizacoes(300);
+        filtro.filtra(episodio);
+
     }
 }
--- src/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
+++ src/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
@@ -1,4 +1,20 @@
 package br.com.alura.screenmatch.calculos;
 
+import br.com.alura.screenmatch.modelos.Titulo;
+
 public class CalculadoraDeTempo {
+    private int tempoTotal;
+
+    public int getTempoTotal() {
+        return tempoTotal;
+    }
+
+    public void setTempoTotal(int tempoTotal) {
+        this.tempoTotal = tempoTotal;
+    }
+
+    public void inclui(Titulo titulo) {
+        System.out.println("Adicionando duração em minutos de " + titulo);
+        this.tempoTotal += titulo.getDuracaoEmMinutos();
+    }
 }
